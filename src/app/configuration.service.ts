c837c7f287cbae590fe16bf6ccdfea0253bb5010
Willer Santos <fernandes.willer.santos@gmail.com>
2019-04-26 00:30:47 -0300

    Método POST (com dados mockados) funcionando ao clicar no botão

--- src/app/configuration.service.ts
+++ src/app/configuration.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,  HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import {ConfigComponent} from './config/config.component';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,17 +10,34 @@ import {ConfigComponent} from './config/config.component';
 export class ConfigurationService {
 
 baseUrl:string = "http://localhost:8080";
-configPath:string = "/config"
+configPath:string = "/config";
+
+ httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+
+ configMock: ConfigComponent;
 
 
   constructor(private httpClient : HttpClient) { }
 
-  getAll(){
+  getAll(): Observable <ConfigComponent[]>{
   	return this.httpClient.get(this.baseUrl + this.configPath);
   }
 
-  save(){
-  	return this.httpClient.post(this.baseUrl + this.configPath);
+  save(): Observable<ConfigComponent>{
+  	//this.configMock.id = 2;
+  	//this.configMock.chave = "teste_angular";
+  	//this.configMock.valor = "cadastrou";
+  	return this.httpClient.post(this.baseUrl + this.configPath, "{\"chave\":\"stop_low\",\"valor\":\"1\"}", this.httpOptions);
   }
+
+  saveFromSite(employee): Observable<ConfigComponent> {
+  
+    return this.httpClient.post<ConfigComponent>(this.baseUrl + this.configPath, JSON.stringify(employee), this.httpOptions);
+  }  
   
 }
