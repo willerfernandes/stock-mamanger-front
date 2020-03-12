c692e5e70dcc34061912b32630ea9fa063ecad2e
BI001732 <willer.santos@bancointer.com.br>
2020-03-11 23:19:29 -0300

    ajuste no tíluto da tela do gráfico de pizza

--- src/app/layouts/expense-dashboard/expense-dashboard.component.ts
+++ src/app/layouts/expense-dashboard/expense-dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {ExpenseService} from './../../services/expense.service';
-import {DateAdapter} from '@angular/material/core';
+import { ExpenseService } from './../../services/expense.service';
+import { DateAdapter } from '@angular/material/core';
 
 import { FormControl } from '@angular/forms';
 import { FakeService } from 'src/app/services/fake.service';
@@ -11,13 +11,22 @@ import { FakeService } from 'src/app/services/fake.service';
 })
 export class ExpenseDashboardComponent implements OnInit {
 
-// Pie
+  // Pie
   public pieChartLabels: string[];
   public pieChartData: number[];
   public pieChartType = 'pie';
+  public pieChartTitle = '';
 
-  public dataInicial;
-  public dataFinal;
+  public startDate: any;
+  public endDate: any;
+
+  monthNames = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio',
+  'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+
+  /*, "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];*/
+
 
   gruposLancamentos;
   tableTitle = 'RESUMO DESPESAS';
@@ -34,19 +43,40 @@ export class ExpenseDashboardComponent implements OnInit {
   }
 
 
+  setCurrentTile() {
+    const startDate: Date = new Date(this.startDate.value.toISOString());
+    const endDate: Date = new Date(this.endDate.value.toISOString());
+    if (startDate.getMonth() === endDate.getMonth() &&
+    startDate.getFullYear() === endDate.getFullYear()) {
+      console.log('mesmo mes e ano');
+      const consideredStartDate = new Date(startDate.getFullYear(), startDate.getMonth(), 1);
+      const consideredEndDate = new Date(startDate.getFullYear(), startDate.getMonth() + 1, 0);
+      this.pieChartTitle = this.monthNames[startDate.getMonth()];
+      if (startDate === consideredStartDate && endDate === consideredEndDate) {
+        console.log('Mesma data');
+        //this.setCurrentTile = this.monthNames[startDate.getMonth()];
+      }
+    } else {
+      this.pieChartTitle = 'Personalizado';
+    }
+
+  }
+
+
   get_expenses_resume(startDate: any, endDate: any) {
+    this.setCurrentTile();
     console.log('Get Expense Resume!');
     this.fakeService.loadExpenseReport(startDate.toISOString(), endDate.toISOString()).subscribe(res => {
-    console.log(res);
-    if (res) {
-      this.pieChartLabels = res.itemGrafico.nome;
-      this.pieChartData = res.itemGrafico.valor;
-      this.gruposLancamentos = res.gruposLancamentos;
-      this.isSuccess = res.itemGrafico !== null;
-    } else {
-      this.isSuccess = false;
-      console.log('No content -> Fill with empty wallet image!');
-    }
+      console.log(res);
+      if (res) {
+        this.pieChartLabels = res.itemGrafico.nome;
+        this.pieChartData = res.itemGrafico.valor;
+        this.gruposLancamentos = res.gruposLancamentos;
+        this.isSuccess = res.itemGrafico !== null;
+      } else {
+        this.isSuccess = false;
+        console.log('No content -> Fill with empty wallet image!');
+      }
     });
   }
 
@@ -54,15 +84,18 @@ export class ExpenseDashboardComponent implements OnInit {
 
   ngOnInit() {
     this.apapter.setLocale('br');
-    const dataInicial = new Date();
-    dataInicial.setMonth(dataInicial.getMonth() - 1);
-    this.dataInicial = new FormControl(dataInicial);
-    this.dataFinal = new FormControl(new Date(Date.now()));
-    this.get_expenses_resume(this.dataInicial.value, this.dataFinal.value);
+    let dataInicial = new Date(Date.now());
+    dataInicial = new Date(dataInicial.getFullYear(), dataInicial.getMonth(), 1);
+
+    let dataFinal = new Date(Date.now());
+    dataFinal = new Date(dataFinal.getFullYear(), dataFinal.getMonth() + 1, 0);
+
+    this.startDate = new FormControl(dataInicial);
+    this.endDate = new FormControl(dataFinal);
+    this.get_expenses_resume(this.startDate.value, this.endDate.value);
   }
 
-  /*openModal(id: string) {
-    this.modalService.open(id);
-}*/
+
+
 
 }
